04e77773063d993dc82ef15aa9816171cd09cd0c
titronius <titronius@gmail.com>
2024-08-13 11:54:39 +0300

    init

---
+++ phones/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    # id           = models.IntegerField()
+    # name         = models.CharField(max_length=100)
+    # price        = models.IntegerField()
+    # image        = models.FilePathField()
+    # release_date = models.DateTimeField()
+    # lte_exists   = models.BooleanField()
+    # slug         = models.CharField(max_length=100)
+    pass
